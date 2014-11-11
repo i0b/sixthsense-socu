56e8f7f1a29b1dc3c9e428919d3f36aa0ac34dc8
cmichi <mich@elmueller.net>
2014-11-11 11:32:29 +0100

    adding basic files

---
+++ socu.js
@@ -0,0 +1,15 @@
+var restify = require('restify');
+var datastreams = [];
+
+function respond(req, res, next) {
+	res.send('hello ' + req.params.name);
+	next();
+}
+
+var server = restify.createServer();
+server.get('/datastreams/:name', respond);
+server.head('/datastreams/:name', respond);
+
+server.listen(8080, function() {
+	console.log('%s listening at %s', server.name, server.url);
+});
